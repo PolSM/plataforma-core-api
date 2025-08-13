adea7077a2c7625eb9b19322893d5cdb28f60917
Pol Sánchez <psanchez.ext@heytrade.com>
2025-08-13 11:39:29 +0200

    refactor: Code cleanup

--- src/main/java/api/application/controllers/PriceController.java
+++ src/main/java/api/application/controllers/PriceController.java
@@ -11,7 +11,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
--- src/main/java/api/infrastructure/persistence/entities/PriceJpaEntity.java
+++ src/main/java/api/infrastructure/persistence/entities/PriceJpaEntity.java
@@ -15,28 +15,20 @@ public class PriceJpaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "product_id")
     private Integer productId;
-
     @Column(name = "brand_id")
     private Integer brandId;
-
     @Column(name = "start_date")
     private LocalDateTime startDate;
-
     @Column(name = "end_date")
     private LocalDateTime endDate;
-
     @Column(name = "price_list")
     private Integer priceList;
-
     @Column(name = "priority")
     private Integer priority;
-
     @Column(name = "price")
     private BigDecimal price;
-
     @Column(name = "currency")
     private String currency;
 }
